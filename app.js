6b9fa5d3c601050bd806ff44963d58640c1e62ab
Kushal525 <58986006+Kushal525@users.noreply.github.com>
2021-03-04 16:51:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const User = require('./models/user')
 const auth = require('./middleware/auth')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 
